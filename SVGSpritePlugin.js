708d6d99b8d7e00b8d6117ead781f31ce637b84c
Guru107 <guruak107@gmail.com>
2016-11-24 16:53:24 +0530

    Sprite Creation, Manifest file generation

--- SVGSpritePlugin.js
+++ SVGSpritePlugin.js
@@ -7,39 +7,97 @@ function SVGSpritePlugin(options){
     this.options = options
 }
 SVGSpritePlugin.prototype.apply = function(compiler) {
-    const source = this.options.source,
-        spriteConfig = this.options.spriteConfig
-    compiler.plugin('this-compilation',function(compilation){
-        console.log('This Compilation')
 
-        compilation.plugin('optimize-assets',function(assets,done){
-            console.log('Optimize Assets')
-           const matchedPaths = glob.sync(source)
-           .map(function(files){
-               return path.join(__dirname,files)
-           })
-           
-          
-           const spriter = SVGSprite(spriteConfig)
-           matchedPaths.forEach(function(filename){
-               spriter.add(filename,null,fs.readFileSync(filename,{encoding:'utf-8'}))
-           })
-           
-           spriter.compile(function(err,result){
-               if(err){
-                   throw err
-               }
-               const contents = result.symbol.sprite.contents.toString('utf-8')
-              console.log('Result: ',contents)
-           })
-           
+
+    compiler.plugin('this-compilation',thiscompilationCallback.bind(this,compiler))
+
+}
+/**
+ *
+ * @param {object} compiler
+ * @param {object} compilation
+ */
+function thiscompilationCallback(compiler,compilation) {
+    compilation.plugin('optimize-assets',optimizeAssetsEventCallback.bind(this,compiler,compilation))
+}
+
+function optimizeAssetsEventCallback(compiler,compilation,assets,done){
+
+    const source = this.options.source,
+        destination = this.options.destination,
+        spriteConfig = this.options.spriteConfig,
+        manifestFile = this.options.manifestFile || 'svg-assets.json',
+        fullManifestPath = path.join(__dirname,compiler.options.output.path +'/'+ manifestFile),
+        _self = this,
+        manifestExists = fs.existsSync(fullManifestPath)
+
+    let manifestObject = {}
+    if(manifestExists){
+        manifestObject = JSON.parse(fs.readFileSync(fullManifestPath,'utf-8'))
+    }
+
+    const matchedPaths = glob.sync(source).map(function(files){
+        return path.join(__dirname,files)
+    })
+
+    const spriter = SVGSprite(spriteConfig)
+
+    matchedPaths.forEach(function(filename){
+        spriter.add(filename,null,fs.readFileSync(filename,{encoding:'utf-8'}))
+    })
+
+    spriter.compile(function(err,result){
+        if(err) {
+            throw err
+        }
+
+        const outputPath = destination ? compiler.options.output.path+'/'+destination : compiler.options.output.path
+
+        compiler.outputFileSystem.mkdir(compiler.options.output.path,function(){
+            compiler.outputFileSystem.mkdir(outputPath,function(){
+                writeAllFiles.call(_self,compiler,compilation,result,spriteConfig,manifestObject,function(response,manifest){
+                    compiler.outputFileSystem.writeFile(fullManifestPath,JSON.stringify(manifest),function(){
+                        done()
+                    })
+                })
+            })
         })
 
     })
 
-    compiler.plugin('done',function(){
-        console.log('Hello World')
-    })
 }
 
+function writeAllFiles(compiler,compilation,result,spriteConfig,manifestObject,callback){
+
+
+   let deferedArray = [],
+           tempManifest = {}
+    const publicPath = compiler.options.output.publicPath ? compiler.options.output.publicPath : '',
+            destination = this.options.destination
+             Object.keys(result).forEach(function(mode){
+
+                for(let type in result[mode]){
+                   tempManifest = Object.assign({},manifestObject,{
+                        [spriteConfig.mode[mode].sprite]: publicPath+destination+'/'+result[mode][type].relative
+                    })
+                    deferedArray.push(writeFile(compiler.outputFileSystem,result[mode][type].path,result[mode][type].contents))
+                }
+
+
+
+            })
+            Promise.all(deferedArray).then(function(response){
+                callback(response[0],tempManifest)
+            })
+}
+
+function writeFile(outputFileSystem,path,contents){
+    return new Promise(function(resolve,reject){
+        outputFileSystem.writeFile(path,contents,function(err){
+            if(err)
+                reject(err)
+            resolve('success')
+        })
+    })
+}
 module.exports = SVGSpritePlugin
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 var SVGSpritePlugin = require('./SVGSpritePlugin')
+var AssetsPlugin = require('assets-webpack-plugin')
+var assetsPluginInstance = new AssetsPlugin({path:'public'})
+
 module.exports = {
     entry:'./index.js',
     output:{
         filename:'bundle.js',
-        path:'public'
+        path:'public',
+        publicPath:'/static/'
     },
     plugins: [
         new SVGSpritePlugin({
-            source:'./svg/**/*.svg',
+            source:'./svg/location/*.svg',
+            'manifestFile':'svg-assets.json',
             spriteConfig:{
                 dest:'public/icons',
                 mode:{
                     symbol:{
-                       sprite:'my-sprite.svg' 
+                       dest:'.',
+                       sprite:'location-sprite.svg',
+                       bust:true
                     }
                 },
                 shape:{
@@ -23,6 +30,28 @@ module.exports = {
                 }
             },
             destination:'icons'
-        })
+        }),
+        new SVGSpritePlugin({
+            source:'./svg/position/*.svg',
+            'manifestFile':'svg-assets.json',
+            spriteConfig:{
+                dest:'public/icons',
+                mode:{
+                    symbol:{
+                       dest:'.',
+                       sprite:'position-sprite.svg',
+                       bust:true
+                    }
+                },
+                shape:{
+                    id:{
+                        generator:'icon-%s'
+                    },
+                    transform:['svgo']
+                }
+            },
+            destination:'icons'
+        }),
+        assetsPluginInstance
     ]
 }
